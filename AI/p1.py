36d74238b6083375ab9e6c57fdec2e1b93315ef5
pashaarshad <arshadpashaintern@gmail.com>
2026-04-16 23:29:35 +0530

    my code

---
+++ AI/p1.py
@@ -0,0 +1,29 @@
+g={
+        'A':['B','C'],
+        'B':['D','E'],
+        'C':['F'],
+        'D':[],
+        'E':['F'],
+        'F':[]
+
+
+    }
+
+v = []
+q = []
+
+def bfs(v,g,n):
+    v.append(n)
+    q.append(n)
+
+    while q:
+        s=q.pop(0)
+        print(s,end="")
+
+        for n in g[s]:
+            if n not in v:
+                v.append(n)
+                q.append(n)
+
+
+bfs(v,g,'A')
