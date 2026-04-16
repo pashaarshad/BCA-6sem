83f05d0a0928fe66e1a90a3be36a0548c7835c4f
pashaarshad <arshadpashaintern@gmail.com>
2026-04-16 23:34:57 +0530

    2ned

---
+++ AI/p2.py
@@ -0,0 +1,18 @@
+g={
+    'A':['B','C'],
+    'B':['D','E'],
+    'C':['F'],
+    'D':[],
+    'E':['F'],
+    'F':[]
+}
+v = set()
+
+def dfs(v,g,n):
+    if n not in v:
+        print(n,end="")
+        v.add(n)
+        for n in g[n]:
+            dfs(v,g,n)
+
+dfs(v,g,'A')
